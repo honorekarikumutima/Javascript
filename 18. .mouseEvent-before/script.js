ff2efeefb381960635024b5c7fe5f04833f6c7ed
honorekarikumutima <honorekarikumutima@gmail.com>
2023-07-17 19:45:41 +0200

    Advanced Javascript

---
+++ 18. .mouseEvent-before/script.js
@@ -0,0 +1,12 @@
+//select element
+const mLeave = document.querySelector(".mLeave");
+const mEnter = document.querySelector(".mEnter");
+const mOver = document.querySelector(".mOver");
+const mOut = document.querySelector(".mOut");
+const mMove = document.querySelector(".mMove");
+const mClick = document.querySelector(".mClick");
+const mDblClick = document.querySelector(".mDblClick");
+const mWheel = document.querySelector(".mWheel");
+const mContext = document.querySelector(".mContext");
+const mUp = document.querySelector(".mUp");
+const msgEl = document.querySelector(".msg");

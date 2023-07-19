1498d3ac1ee7239babd47eea442c51fe06d03a2b
honorekarikumutima <honorekarikumutima@gmail.com>
2023-07-19 12:05:33 +0200

    Advanced Javascript

---
+++ 20.keyboardEvents-before/script.js
@@ -0,0 +1,2 @@
+//select h1
+const h1 = document.querySelector("h1");
---
+++ 22.formEvents-before/script.js
@@ -0,0 +1,7 @@
+//Select elements
+const pName = document.querySelector(".pName");
+const pPrice = document.querySelector(".pPrice");
+const pDesc = document.querySelector(".pDesc");
+const pQty = document.querySelector(".pQty");
+const category = document.querySelector(".category");
+const msg = document.querySelector(".msg");
